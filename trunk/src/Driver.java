0bbb0d32ca901c8c3668a0a6c42dd647eed6a618
j.v.trigueros <j.v.trigueros@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-12 02:02:59 +0000

    Altered DatabaseCreator to create the Database and Table for Phase 3. It was modified so that it uses static methods, so that we don't have to create an object. I removed a lot of the stuff that we did no longer needed and made a few changes ( don't get mad, I promise it works ).

---
+++ trunk/src/Driver.java
@@ -0,0 +1,20 @@
+/**
+ * Run Simple Test Cases
+ */
+
+/**
+ * Run Simple Test Cases
+ * @author Jose Trigueros
+ *
+ */
+public class Driver
+{
+    /**
+     * @param args 
+     */
+    public static void main(String[] args)
+    {
+        // Just testing to see if the DatabaseCreator Works
+        DatabaseCreator.createPhase3Database("");
+    }
+}
