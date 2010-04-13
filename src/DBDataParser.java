6d7286a4e477bf7eef90cdc82020ee4bc2b80871
j.v.trigueros <j.v.trigueros@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-13 00:21:10 +0000

    The DBDataParser gets the data directly from an xls file and puts it into the database.

--- src/DBDataParser.java
+++ src/DBDataParser.java
@@ -1,48 +1,91 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
 
-public class DBDataParser {
-	public DBDataParser() {
-		
-	}
-	
-	public static void populateDatabase(Statement stmt,
-										String fileName) {
-		File file = new File( fileName );
-		BufferedReader in = null;
-		try {
-			in = new BufferedReader( new FileReader ( file ) );
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return;
-		}
-		
-		try {
-			String line;
-			while ((line = in.readLine( )) != null) {
-				String[] data = line.split(";");
-				String sql = "INSERT INTO Student " +
-							 "VALUES('" + data[0] + "','" + data[1] + "','" +
-							              data[2] + "','" + data[3] + "','" +
-							              data[4] + "','" + data[5] + "')";
-				stmt.executeUpdate(sql);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			in.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+
+public class DBDataParser
+{
+    // Global Variables
+    private static final int NUM_COLS     = 6;
+    private static final int PRODUCT_ID   = 0,
+                             PRICE        = 1,
+                             DEPT_ID      = 2,
+                             WEIGHT       = 3,
+                             PRODUCT_YEAR = 4,
+                             EXPIRE_YEAR  = 5;
+    
+    //TODO: Do we really need this Constructor?
+    public DBDataParser()
+    {
+
+    }
+
+    /**
+     * Reads an excel file and collects the information putting it in a database.
+     * @param statement 
+     * @param filename
+     */
+    public static void populateDatabase( Statement statement, String filename )
+    {
+        String[] rowBuffer = new String[NUM_COLS];
+        File inputExcel = new File(filename);
+        Workbook workbook;
+        
+        try
+        {
+            workbook = Workbook.getWorkbook(inputExcel);
+            
+            // Assuming that all the data is contained is the first sheet
+            Sheet sheet = workbook.getSheet(0);
+            
+            // Loop through all the contents reading them into a string buffer
+            // We skip the first row because that should contain the headings
+            // Note: sheet.getCell(COL,ROW)
+            for( int i = 1; i < sheet.getRows() ; i++ )
+            {
+                for( int j = 0; j < sheet.getColumns() ; j++ )
+                {
+                    // Store Values into the row buffer
+                    rowBuffer[j] = sheet.getCell(j,i).getContents(); 
+                }
+                
+                // Add a batch job for the connection
+                statement.addBatch("INSERT INTO Student " + 
+                                   "VALUES('" + 
+                                   rowBuffer[PRODUCT_ID] + "','" + 
+                                   rowBuffer[PRICE] + "','" + 
+                                   rowBuffer[DEPT_ID] + "','" + 
+                                   rowBuffer[WEIGHT] + "','" + 
+                                   rowBuffer[PRODUCT_YEAR] + "','" + 
+                                   rowBuffer[EXPIRE_YEAR] + "')");
+            }
+            
+            // Once all the rows have been added to the batch we can run all those queries in one go
+            statement.executeBatch();
+            
+        } catch (SQLException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (BiffException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
 }
