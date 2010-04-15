fa39d4130666ba9d367bd5ffcfda48100637446c
k2eldorado <k2eldorado@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-15 23:53:07 +0000

    QuasiIdSelector contains the information about which attributes are selected for generalization. QuasiId is an enum class that should help out a lot, max gen is hard-coded there (but we need to discover those values still). Samarati class was changed to reflect the new classes.

--- src/QuasiId.java
+++ src/QuasiId.java
@@ -1,8 +1,26 @@
 
 public enum QuasiId {
-	PRODUCT_ID,
-	PRICE,DEPT_ID,
-	WEIGHT,
-	PRODUCT_YEAR,
-	EXPIRE_YEAR;
+	// TODO: need to set these max gens to the correct value
+	PRODUCT_ID(2, "`productID`"),
+	PRICE(2,"`price`"),
+	DEPT_ID(2, "`deptID`"),
+	WEIGHT(2, "`weight`"),
+	PRODUCT_YEAR(2, "`productYear`"),
+	EXPIRE_YEAR(2, "`expireYear`");
+	
+	final int maxGeneralization;
+	final String dbName;
+	
+	QuasiId(int maxGeneralization, String dbName) {
+		this.maxGeneralization = maxGeneralization;
+		this.dbName = dbName;
+	}
+	
+	public String getDBName() {
+		return dbName;
+	}
+	
+	public int getMaxGen() {
+		return maxGeneralization;
+	}
 }
