32dbb18ddaecbeb80d8320f67aec556c67c1ed23
k2eldorado <k2eldorado@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-16 23:56:17 +0000

    working on the matching clusters method.  Its not finished yet, please don't touch

--- trunk/src/GeneralizationTable.java
+++ trunk/src/GeneralizationTable.java
@@ -3,19 +3,20 @@ import java.util.ArrayList;
 
 public class GeneralizationTable {
 	
-	public GeneralizationTable(DBManager dbManager, QuasiId ... list) {
-//		this.dbManager = dbManager;
+	public GeneralizationTable(QuasiId ... list) {
 		this.genTable = new ArrayList<GeneralizationCell>();
-		this.selectIds = list;
+		this.selectedIds = list;
 		this.setClusterList();
-		this.fillGenTable();
+		this.setSingleTupleList();
+//		this.fillGenTable();
 	}
-	
+
 	// some optimizations can happen here.  this is just initial code
 	public boolean testSolution(GeneralizationSteps solution,
 								int kAnonymity,
 								int maxSuppression,
 								boolean getTableData) {
+//		DBManager dbManager = new DBManager();
 		int suppressionCount = 0;
 		int lineKanonymity = 0;
 		String currentRow = genTable.get(0).getSingleTuple();
@@ -30,6 +31,7 @@ public class GeneralizationTable {
 					// we didn't meet the kanon for the last row
 					suppressionCount++;
 					if (suppressionCount > maxSuppression) {
+//						dbManager.closeConnection(false);
 						return false;	// we broke the max suppression limit
 					}
 				}
@@ -44,6 +46,7 @@ public class GeneralizationTable {
 				
 			}
 		}
+//		dbManager.closeConnection(false);
 		return true;
 	}
 	
@@ -85,23 +88,119 @@ public class GeneralizationTable {
 	
 	private void fillGenRow(String singleTuple) {
 		for (String s : clusterList) {
-			genTable.add(new GeneralizationCell(singleTuple, s, selectIds));
+			genTable.add(new GeneralizationCell(singleTuple, s, selectedIds));
 		}
 	}
 
 	private void setClusterList() {
-		// TODO: finish method
-		this.clusterList = new String[6];
-		for (int i = 0; i < 6; i++) {
-			clusterList[i] = "t" + i;
+		// get a DBManager
+		DBManager dbManager = new DBManager();
+		
+		String allQuasiIds = getCommaSeparatedQuasiIdList();
+		String alreadyMatched = "----";
+		
+		// create sql to get all ProductIds
+		String sql = "SELECT " + QuasiId.PRODUCT_ID.dbName + " " +
+					 "FROM Student";
+		
+		// place all ProductIds into string array
+		String[] allProductIds = dbManager.runQuery(sql);
+		
+//		// test output
+//		for (int i = 0; i < allProductIds.length; i++) {
+//			System.out.println(allProductIds[i]);
+//		}
+		
+		// test each id in ProductIds array to find matches.
+		int productIdPtr = 0;
+		while ( productIdPtr < allProductIds.length ) {
+			
+			// get the next productId to find samples for
+			String sampleProductId = allProductIds[productIdPtr];
+			allProductIds[productIdPtr] = alreadyMatched; // set to "----"
+			
+			// get the quasiId values for the sample
+			sql = "SELECT " + allQuasiIds + " " +
+				  "FROM Student " +
+				  "WHERE " + QuasiId.PRODUCT_ID.getDBName() + "='" + sampleProductId + "'";	
+			String[] sampleProductIdQuasiIdValues = dbManager.runQuery(sql);
+
+//			System.out.println("values for " + sampleProductId);
+//			for (String s : sampleProductIdQuasiIdValues) {
+//				System.out.println(s);
+//			}
+			
+			// get the product id(s) for everything that matches the values
+			// found above
+			sql = "SELECT " + QuasiId.PRODUCT_ID.getDBName() + " " +
+				  "FROM Student " +
+				  "WHERE " + getQuasiIdValuePairs(sampleProductIdQuasiIdValues, sampleProductId);
+			String[] matchingProducts = dbManager.runQuery(sql);
+			
+			// set matching productIds in allProductIds array to "----"
+			// we don't want to get duplicate data and re-match again
+			if (matchingProducts != null) {
+				for (String s : matchingProducts) {
+					for (int i = 0; i < allProductIds.length; i++) {
+						if (allProductIds[i].equals(s)) {
+							allProductIds[i] = alreadyMatched;
+							i = allProductIds.length; // found it, break the loop
+						}
+					}
+				}
+			}
+
+			if (matchingProducts != null) {
+				for (int i = 0; i < matchingProducts.length; i++) {
+					System.out.println(sampleProductId + " " + matchingProducts[i]);
+				}				
+			}
+			
+			productIdPtr++;
+
+			
+			// iterate to the next index that is not "----" and be sure
+			// to stay within length of allProductIds array
+			while ( (productIdPtr < allProductIds.length) &&
+					(allProductIds[productIdPtr].equals(alreadyMatched)) )  {
+				productIdPtr++; // advance the pointer, index is matched already
+			}
 		}
+		
+		// close that db connection...
+		dbManager.closeConnection(false);		
+	}
+	
+	private String getQuasiIdValuePairs(String[] values, String samplePid) {
+		String output = "";
+		for (int i = 0; i < selectedIds.length; i++) {
+			output += " AND " + selectedIds[i].getDBName() + "='" + values[i] + "'";
+		}
+		output += " AND " + QuasiId.PRODUCT_ID.getDBName() + "!='" + samplePid + "'";
+		output = output.substring(5);
+		return output;		
+	}
+	
+	private String getCommaSeparatedQuasiIdList() {
+		String allQuasiIds = "";
+		for (QuasiId id : this.selectedIds) {
+			allQuasiIds +=  "," + id.getDBName();
+		}
+		allQuasiIds = allQuasiIds.substring(1); // remove first comma
+		return allQuasiIds;
+	}
+	
+	private void setSingleTupleList() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	private String[] clusterList;
-	private QuasiId[] selectIds;
+	private String[] singleTupleList;
+	private QuasiId[] selectedIds;
 	private ArrayList<GeneralizationCell> genTable;
-	private DBManager dbManager;
-
+	
+	
 	private class GeneralizationCell {
 		private String singleTuple;
 		private String targetCluster;
