2f22535d2d89a253d6d6b950b3e64f1d4cdfbebf
k2eldorado <k2eldorado@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-16 01:45:42 +0000

    Generalization steps updated to contain an array of quasi-id/step pairs. Dominates method was rewritten.

--- src/GeneralizationSteps.java
+++ src/GeneralizationSteps.java
@@ -1,110 +1,84 @@
-/**
- * This is really the QuasiIdentifiers class with each non-negative
- * position representing the number of generalization steps for 
- * that position. I only sub-classed it to create a distinction 
- * between the two classes and make a prettier toString() for it.
- * This is where the dominates() method happens also.
- *
- */
-public class GeneralizationSteps extends QuasiIdentifiers {
+public class GeneralizationSteps {
 	
 	public GeneralizationSteps() {
-		productId = -1;
-		price = -1;
-		weight = -1;
-		deptId = -1;
-		productYear = -1;
-		expireYear = -1;
+		dataPairs = new GeneralizationStepsEntry[QuasiId.values().length];
+		
+		for (int i = 0; i < QuasiId.values().length; i++) {
+			dataPairs[i] = new GeneralizationStepsEntry();
+		}
+
+		dataPairs[PRODUCT_ID_POSITION].setAttributeId(QuasiId.PRODUCT_ID);
+		dataPairs[PRICE_POSITION].setAttributeId(QuasiId.PRICE);
+		dataPairs[WEIGHT_POSITION].setAttributeId(QuasiId.WEIGHT);
+		dataPairs[DEPT_ID_POSITION].setAttributeId(QuasiId.DEPT_ID);
+		dataPairs[PRODUCT_YEAR_POSITION].setAttributeId(QuasiId.PRODUCT_YEAR);
+		dataPairs[EXPIRE_YEAR_POSITION].setAttributeId(QuasiId.EXPIRE_YEAR);
 	}
 	
-	public int getQuasiId(QuasiId id) {
+	// if id is already in vector, the old steps value become
+	// this step value...
+	public void setGenSteps(QuasiId id, int steps) {
 		switch (id) {
 			case DEPT_ID:
-				return this.deptId;
+				dataPairs[DEPT_ID_POSITION].setNumSteps(steps);
+				break; 
 			case EXPIRE_YEAR:
-				return this.expireYear;
+				dataPairs[EXPIRE_YEAR_POSITION].setNumSteps(steps);
+				break;
 			case PRICE:
-				return this.price;
+				dataPairs[PRICE_POSITION].setNumSteps(steps);
+				break;
 			case PRODUCT_ID:
-				return this.productId;
-			case PRODUCT_YEAR:
-				return this.productYear;
+				dataPairs[PRODUCT_ID_POSITION].setNumSteps(steps);
+				break;
 			case WEIGHT:
-				return this.weight;
+				dataPairs[WEIGHT_POSITION].setNumSteps(steps);
+				break;
+			case PRODUCT_YEAR:
+				dataPairs[PRODUCT_YEAR_POSITION].setNumSteps(steps);
+				break;
+		}
+	}
+	
+	public int getGenStepValue(QuasiId id) {
+		switch (id) {
+			case DEPT_ID:
+				return dataPairs[DEPT_ID_POSITION].getNumSteps();
+			case EXPIRE_YEAR:
+				return dataPairs[EXPIRE_YEAR_POSITION].getNumSteps();
+			case PRICE:
+				return dataPairs[PRICE_POSITION].getNumSteps();
+			case PRODUCT_ID:
+				return dataPairs[PRODUCT_ID_POSITION].getNumSteps();
+			case WEIGHT:
+				return dataPairs[WEIGHT_POSITION].getNumSteps();
+			case PRODUCT_YEAR:
+				return dataPairs[PRODUCT_YEAR_POSITION].getNumSteps();
 			default:
 				return -1;
-		}	
-	}
-	
-	public void setQuasiId(QuasiId id, int value) {
-		switch (id) {
-		case DEPT_ID:
-			this.deptId = value;
-			break;
-		case EXPIRE_YEAR:
-			this.expireYear = value;
-			break;
-		case PRICE:
-			this.price = value;
-			break;
-		case PRODUCT_ID:
-			this.productId = value;
-			break;
-		case PRODUCT_YEAR:
-			this.productYear = value;
-			break;
-		case WEIGHT:
-			this.weight = value;
-			break;
-		}		
-	}
-	
-	private int[] getSelected() {
-		int[] selected = new int[QuasiId.values().length];
-		int marker = 0;
-		for (QuasiId id : QuasiId.values()) {
-			selected[marker] = this.getQuasiId(id);
-			marker++;
-		}
-		return selected;		
-	}
-	
-	public boolean equals(GeneralizationSteps other) {
-		if ( !(other instanceof GeneralizationSteps) ) {
-			return false;
-		} else {
-			int[] values1 = this.getSelected();
-			int[] values2 = other.getSelected();
-			for (int i = 0; i < QuasiId.values().length; i++) {
-				if ( !(values1[i] == values2[i]) ) {
-					return false;
-				}
-			}
-			return true;
 		}
 	}
 	
-	public String toString() {
+	public GeneralizationStepsEntry[] getDataPairs() {
+		return this.dataPairs;
+	}
+	
+	public String viewStepsOnly() {
 		String output = "[ ";
-		if (productId > -1) {
-			output += QuasiId.PRODUCT_ID + " ";
-		}
-		if (deptId > -1) {
-			output += QuasiId.DEPT_ID + " ";
-		}
-		if (price > -1) {
-			output += QuasiId.PRICE + " ";
-		}
-		if (weight > -1) {
-			output += QuasiId.WEIGHT + " ";
-		}
-		if (productYear > -1) {
-			output += QuasiId.PRODUCT_YEAR + " ";
-		}
-		if (expireYear > -1) {
-			output += QuasiId.EXPIRE_YEAR + " ";
+		for (GeneralizationStepsEntry entry : dataPairs) {
+			if (entry.getNumSteps() > -1) {
+				output += entry.getNumSteps() + " ";				
+			}
 		}
 		output += "]";
+		return output;		
+	}
+
+	public String toString() {
+		String output = "";
+		for (GeneralizationStepsEntry entry : dataPairs) {
+			output += entry.toString() + "\n";
+		}
 		return output;
 	}
 	
@@ -114,20 +88,63 @@ public class GeneralizationSteps extends QuasiIdentifiers {
 	 * of the possibleSolution is less than or equal to each index
 	 * of the calling object's quasi-id list.  
 	 * 
-	 * @param possibleSolution	int array of quasi-id steps to test
+	 * @param possibleSolution	an array of quasi-id steps to test
 	 * 							against.
 	 * @return					True if all indices of 
-	 * 							possibleSolution is less than or 
+	 * 							possible solution is less than or 
 	 * 							equal to the quasi-id list.
 	 */
-	public boolean dominates(GeneralizationSteps other) {
-		return false;
+	public boolean dominates(GeneralizationStepsEntry[] other) {
+		int marker = 0;
+		for (GeneralizationStepsEntry entry : dataPairs) {
+			if (entry.getNumSteps() < other[marker].getNumSteps()) {
+				return false;
+			}
+			marker++;
+		}
+		return true;
 	}
 	
-	private int productId;
-	private int price;
-	private int weight;
-	private int deptId;
-	private int productYear;
-	private int expireYear;
+	private GeneralizationStepsEntry[] dataPairs;
+	private static int PRODUCT_ID_POSITION = 0;
+	private static int PRICE_POSITION = 1;
+	private static int WEIGHT_POSITION = 2;
+	private static int DEPT_ID_POSITION = 3;
+	private static int PRODUCT_YEAR_POSITION = 4;
+	private static int EXPIRE_YEAR_POSITION = 5;
+	
+	private class GeneralizationStepsEntry {
+		private QuasiId attributeId;
+		private int numSteps;
+		
+		public GeneralizationStepsEntry() {
+			this.attributeId = QuasiId.DEPT_ID;
+			this.numSteps = -1;
+		}
+		
+		public GeneralizationStepsEntry(QuasiId id, int numSteps) {
+			this.attributeId = id;
+			this.numSteps = numSteps;
+		}
+		
+		public String toString() {
+			return this.attributeId + ": " + this.numSteps;
+		}
+
+		public void setAttributeId(QuasiId attributeId) {
+			this.attributeId = attributeId;
+		}
+
+		public void setNumSteps(int numSteps) {
+			this.numSteps = numSteps;
+		}
+
+		public QuasiId getAttributeId() {
+			return attributeId;
+		}
+
+		public int getNumSteps() {
+			return numSteps;
+		}
+	}
 }
