65a4523f5b03e7693eec016d5038a54a9719f838
k2eldorado <k2eldorado@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-17 21:33:20 +0000

    fixed the enum like we just talked about

--- src/QuasiId.java
+++ src/QuasiId.java
@@ -1,19 +1,25 @@
 
 public enum QuasiId {
 	// TODO: need to set these max gens to the correct value
-	PRODUCT_ID(4, "`productID`"),
-	PRICE(5,"`price`"),
-	DEPT_ID(5, "`deptID`"),
-	WEIGHT(4, "`weight`"),
-	PRODUCT_YEAR(2, "`productYear`"),
-	EXPIRE_YEAR(2, "`expireYear`");
+	PRODUCT_ID(4, "`productID`", 0),
+	PRICE(5,"`price`", 1),
+	DEPT_ID(5, "`deptID`", 2),
+	WEIGHT(4, "`weight`", 3),
+	PRODUCT_YEAR(2, "`productYear`", 4),
+	EXPIRE_YEAR(2, "`expireYear`", 5);
 	
 	final int maxGeneralization;
 	final String dbName;
+	final int position;
 	
-	QuasiId(int maxGeneralization, String dbName) {
+	QuasiId(int maxGeneralization, String dbName, int position) {
 		this.maxGeneralization = maxGeneralization;
 		this.dbName = dbName;
+		this.position = position;
+	}
+	
+	public int getPosition() {
+		return position;
 	}
 	
 	public String getDBName() {
