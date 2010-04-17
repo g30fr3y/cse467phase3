4677c006db9657557cc4b9a56f0a29c82c7165cb
j.v.trigueros <j.v.trigueros@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-17 21:01:47 +0000

    Changed the way the Generalize method is called, we should only generalize one attribute at a time.

--- src/Generalizer.java
+++ src/Generalizer.java
@@ -1,5 +1,5 @@
 /**
- * Generalizes attributes 
+ * Generalizes attributes of a single row or a cluster
  */
 
 
@@ -10,6 +10,7 @@
 public class Generalizer
 {
 // Find a better way to create these global ID
+    
     // Index names
     static final int PRODUCTID   = 0, 
                      PRICE       = 1, 
@@ -20,24 +21,28 @@ public class Generalizer
     /**
      * 
      * @param data A 2d array containing the rows that need to be generalized
-     * @param attributes An array containing the IDs of the attributes to generalize  
+     * @param attribute The name of the attribute to generalize
      * @return A 2d array containing the generalized data.
      */
-    public static String[][] Generalize(String[][] data, int[] attributes)
+    public static String[][] Generalize(String[][] data, int attribute)
     {
         // Maybe not needed
         String[][] generalizedData = data;
         
-        for( int attribute = 0 ; attribute < attributes.length ; attribute++ )
+        switch(attribute)
         {
-            switch(attributes[attribute])
-            {
-                case PRODUCTID:
-                    // I'm stopping here
-                    break;
-                    
-            }
+            case PRODUCTID:
+                generalizeProductID(generalizedData);
+                break;
+
         }
+        
+        return generalizedData;
+    }
+    
+    public static String[][] generalizeProductID( String [][] generalizedData)
+    {
+        // Determine it's level of generalization
         
         return generalizedData;
     }
