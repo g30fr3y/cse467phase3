99cd15fde064d324c705de0cdad3694af527ed18
j.v.trigueros <j.v.trigueros@22eb1cd5-011f-8159-2bb5-fff165cd2b58>
2010-04-20 21:48:37 +0000

    Added a way to create a possible solution set.

--- src/Driver.java
+++ src/Driver.java
@@ -2,12 +2,19 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 public class Driver
 {
 
     public static void main(String[] args)
     {
+        // Test out the permutations
+        int[][] testing = Samarati.createPossibleSolutions( 3, 2 );
+        
+        
+        return;
+    }
 //        QuasiId id = QuasiId.EXPIRE_YEAR;
 //        String entry = "2000";
 //        String entry2 = "0";
@@ -63,8 +70,79 @@ public class Driver
         // System.out.println(Generalizer.getGeneralizedData(solution));
 
 //        Samarati.kanon( 0, 0, QuasiId.DEPT_ID, QuasiId.PRODUCT_ID );
-        Samarati sam = new Samarati( QuasiId.PRODUCT_ID, QuasiId.PRICE );
+//        Samarati sam = new Samarati( QuasiId.PRODUCT_ID, QuasiId.PRICE );
 //        System.out.println(sam.latticeHeight);
-    }
+        
+        // Combinations Example
+//        System.out.println(" Combinations Example ");
+//        int maxSum = 3;
+//        int numTerms = 2;
+//        int cumSum = 0;
+//        int[] indices1;
+//        int[] elements1 = {3 , 2 , 1 , 0};
+//        Vector<int[]> validCombinations = new Vector<int[]>();
+//        
+//        // Generate combinations
+//        CombinationGenerator x1 = new CombinationGenerator( elements1.length, numTerms );
+//        
+//        int k = 0;
+//        // iterate through possible combinations
+//        while (x1.hasMore())
+//        {
+//            // returns an array 
+//            indices1 = x1.getNext();
+//            
+//            for ( int i = 0; i < indices1.length; i++ )
+//            {
+//                // add the values
+//                cumSum += elements1[indices1[i]];
+////                combination.append( elements1[indices1[i]] );
+//            }
+//            
+//            if ( cumSum == maxSum )
+//            {
+//                validCombinations.add( indices1.clone() );
+//                for( int i = 0; i < numTerms ; i++ )
+//                    System.out.print( validCombinations.elementAt(k)[i] );
+//                System.out.println();
+//                k++;
+//            }
+//            
+//            cumSum = 0;
+//        }
+//        
+//        
+//        // From the validCombos we must add to the possible permutations
+//        System.out.println(" Permutations Example ");
+//        
+//        Vector<int[]> possiblePermutations = new Vector<int[]>();
+//        
+//        for( int combination = 0; combination < validCombinations.size() ; combination++ )
+//        {
+//            int[] indices;
+//            int[] elements = validCombinations.elementAt( combination );
+//            
+//            PermutationGenerator x = new PermutationGenerator( elements.length  );
+//            
+//            while (x.hasMore())
+//            {
+//                indices = x.getNext();
+//                // Store actual values
+//                possiblePermutations.add( new int[indices.length] );
+//                for( int i = 0; i < indices.length ; i++ )
+//                {
+//                    possiblePermutations.lastElement()[i] = elements[indices[i]];
+//                }
+//            }
+//        }
+//        
+//        // Print out all the permutations
+//        for( int i = 0; i < possiblePermutations.size() ; i++ )
+//        {
+//            for( int j = 0; j < possiblePermutations.elementAt( i ).length ; j++)
+//                System.out.print( possiblePermutations.elementAt( i )[j] + " ");
+//            System.out.println();
+//        }
+//    }
 
 }
